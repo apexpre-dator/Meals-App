f373ae6b033ba89b725cf49005313216ddaca43e
apexpre-dator <96236927+apexpre-dator@users.noreply.github.com>
2022-03-01 18:06:10 +0530

    Adding to Main Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './categories_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -10,26 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Meals App'),
-      ),
-      body: Center(
-        child: Text('Navigation Time'),
-      ),
+      home: CategoriesScreen(),
     );
   }
 }
