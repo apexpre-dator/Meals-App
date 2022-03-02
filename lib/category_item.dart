29b4c00d350ec992c3141d6f32b7dd9512b7074c
apexpre-dator <96236927+apexpre-dator@users.noreply.github.com>
2022-03-02 14:13:18 +0530

    Using Named Routes

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import './category_meals_screen.dart';
-
 class CategoryItem extends StatelessWidget {
   final String id;
   final String title;
@@ -10,12 +8,12 @@ class CategoryItem extends StatelessWidget {
   CategoryItem(this.id, this.title, this.color);
 
   void selectCategory(BuildContext ctx) {
-    Navigator.of(ctx).push(
-      MaterialPageRoute(
-        builder: (_) {
-          return CategoryMealsScreen(id, title);
-        },
-      ),
+    Navigator.of(ctx).pushNamed(
+      '/category-meals',
+      arguments: {
+        'id': id,
+        'title': title,
+      },
     );
   }
 
--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  final String categoryId;
-  final String categoryTitle;
-
-  CategoryMealsScreen(this.categoryId, this.categoryTitle);
+  const CategoryMealsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
     return Scaffold(
       appBar: AppBar(
-        title: Text(categoryTitle),
+        title: Text(categoryTitle!),
       ),
       body: Center(
         child: Text(
