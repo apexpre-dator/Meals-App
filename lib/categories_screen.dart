86f87fbbe3393b1aeb1091d6994b50600db46c60
apexpre-dator <96236927+apexpre-dator@users.noreply.github.com>
2022-03-02 12:36:15 +0530

    Styling & Theming

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -13,6 +13,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Meals App'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map((catData) => CategoryItem(
                   catData.title,
