b1e3f7a0ce3bf2197b4b14bf41881f77240b8ec8
apexpre-dator <96236927+apexpre-dator@users.noreply.github.com>
2022-03-03 07:30:12 +0530

    Adding Meal Model and Data

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './dummy_data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
   const CategoryMealsScreen({Key? key}) : super(key: key);
 
@@ -11,15 +13,18 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
-      appBar: AppBar(
-        title: Text(categoryTitle!),
-      ),
-      body: Center(
-        child: Text(
-          'Recipes',
+        appBar: AppBar(
+          title: Text(categoryTitle!),
         ),
-      ),
-    );
+        body: ListView.builder(
+          itemBuilder: ((ctx, index) {
+            return Text(categoryMeals[index].title);
+          }),
+          itemCount: categoryMeals.length,
+        ));
   }
 }
