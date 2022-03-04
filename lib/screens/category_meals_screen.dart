f3d6db9ed69ee644f792fedaecf5c773056ddd59
apexpre-dator <96236927+apexpre-dator@users.noreply.github.com>
2022-03-04 14:22:00 +0530

    Displaying Image and Code Refactoring

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './dummy_data.dart';
+import '../widgets/meal_item.dart';
+import '../dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   const CategoryMealsScreen({Key? key}) : super(key: key);
@@ -17,14 +18,21 @@ class CategoryMealsScreen extends StatelessWidget {
       return meal.categories.contains(categoryId);
     }).toList();
     return Scaffold(
-        appBar: AppBar(
-          title: Text(categoryTitle!),
-        ),
-        body: ListView.builder(
-          itemBuilder: ((ctx, index) {
-            return Text(categoryMeals[index].title);
-          }),
-          itemCount: categoryMeals.length,
-        ));
+      appBar: AppBar(
+        title: Text(categoryTitle!),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            title: categoryMeals[index].title,
+            imageUrl: categoryMeals[index].imageUrl,
+            duration: categoryMeals[index].duration,
+            complexity: categoryMeals[index].complexity,
+            affordability: categoryMeals[index].affordability,
+          );
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
   }
 }
