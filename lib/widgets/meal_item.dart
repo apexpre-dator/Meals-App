e56b9071cd66b7f511f7d0c17928b69940fec8c1
apexpre-dator <96236927+apexpre-dator@users.noreply.github.com>
2022-03-04 18:22:34 +0530

    Adding Meal Detail Screen

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 
+import '../screens/meal_detail_screen.dart';
 import '../models/meal.dart';
 
 class MealItem extends StatelessWidget {
-  final String title, imageUrl;
+  final String id, title, imageUrl;
   final int duration;
   final Complexity complexity;
   final Affordability affordability;
 
   MealItem({
+    required this.id,
     required this.title,
     required this.imageUrl,
     required this.duration,
@@ -43,12 +44,17 @@ class MealItem extends StatelessWidget {
     }
   }
 
-  void selectMeal() {}
+  void selectMeal(BuildContext context) {
+    Navigator.of(context).pushNamed(
+      MealDetailScreen.routeName,
+      arguments: id,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: selectMeal,
+      onTap: () => selectMeal(context),
       child: Card(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15),
