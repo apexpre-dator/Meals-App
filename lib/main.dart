7457554ac60134f77ce80f1ee130f277ba32ce90
apexpre-dator <96236927+apexpre-dator@users.noreply.github.com>
2022-03-06 12:18:35 +0530

    Added Top TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
+import './screens/tab_screen.dart';
 import './screens/meal_detail_screen.dart';
 import './screens/category_meals_screen.dart';
-import './screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (ctx) => CategoriesScreen(),
+        '/': (ctx) => TabScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
---
+++ lib/screens/tab_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/screens/favourites_screen.dart';
+
+import './categories_screen.dart';
+
+class TabScreen extends StatefulWidget {
+  const TabScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TabScreen> createState() => _TabScreenState();
+}
+
+class _TabScreenState extends State<TabScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Meal'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.category_sharp),
+                text: 'Categories',
+              ),
+              Tab(
+                icon: Icon(Icons.star_rate_rounded),
+                text: 'Favourites',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            CategoriesScreen(),
+            FavouritesScreen(),
+          ],
+        ),
+      ),
+    );
+  }
+}
